3d6178c5fb9f3d620c11f92355cd80fc19668e5e
Pumpinator <alejandrodcardona5@gmail.com>
2025-08-11 21:43:47 -0600

    inferencias de tipo error manejadas

--- Program.cs
+++ Program.cs
@@ -58,7 +58,12 @@ builder.Services.AddCors(options =>
     });
 
 });
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+        options.JsonSerializerOptions.PropertyNamingPolicy = System.Text.Json.JsonNamingPolicy.CamelCase;
+    });
 builder.Services.AddOpenApi();
 builder.Services.AddHealthChecks();
 
